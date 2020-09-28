163addcbd5282be8635e5e154f8413944195309e
luisbueno8 <51271382+luisbueno8@users.noreply.github.com>
2020-09-29 01:02:04 +0200

    Update and rename hola.php to hola2.py

---
+++ hola2.py
@@ -0,0 +1,29 @@
+
+import telebot     # Biblioteca para usar la API de telegram
+import os          # Para utilizar las variables de entorno del sistema.
+
+TOKEN = os.environ['1378072574:AAFBdIoiS0s3omWEo_StBNFA_GnFN0iizrY']
+
+# Creamos la instancia del bot
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    # Obtenemos el identificador del usuario
+    chat_id = message.chat.id
+    # Se envía el mensaje a dicho usuario
+    bot.send_message(chat_id, "Hola mundo!")
+
+
+# Será llamado si no se ha llamado a ningún otro método
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    # Obtenemos el identificador del usuario
+    chat_id = message.chat.id
+    # Se envía el mensaje a dicho usuario
+    bot.send_message(chat_id, "Este bot solo funciona con los comandos /start y /help")
+
+print("El bot se está ejecutando!")
+
+# Ejecutamos el programa
+bot.polling()
