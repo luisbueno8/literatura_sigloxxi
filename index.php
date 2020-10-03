3f6d2e8dc51908e49c7bac763e7476c3b56410c9
luisbueno8 <51271382+luisbueno8@users.noreply.github.com>
2020-10-03 20:18:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $chatId = $update["message"]["chat"]["id"];
 $pepe2 = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/cartel1.jpg" -F caption="TheImage" > /dev/null &';
 
 $espi_inicio2 = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/espi2.jpeg" -F caption="Y amigo de Don Pimpón 💜" > /dev/null &';
-$img_asterix = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/asterix.jpg" -F caption="Asterix y Obelix" > /dev/null &';
+$img_asterix  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/asterix.jpg" -F caption="Asterix y Obelix" > /dev/null &';
 
 
 $chatType = $update["message"]["chat"]["type"];
@@ -106,7 +106,9 @@ switch ($command) {
   
    case 'asterix y obelix':
    case 'Asterix y Obelix':
-echo exec($img_asterix);
+echo exec ($img_asterix);
+echo exec ($espi_inicio2);
+
   break;
   
   
