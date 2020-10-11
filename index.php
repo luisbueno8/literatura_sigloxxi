4afe98411e685fc4a3cb11fb68310235b4b36d10
luisbueno8 <51271382+luisbueno8@users.noreply.github.com>
2020-10-11 19:01:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ $img_asterix  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_
 $aldea  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/lupaasterix-1200x813.jpg" -F caption="El enemigo de la aldea gala nos permitirá conocer el siguiente códido de codificación..." > /dev/null &';
 $julio  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/julio.jpeg" -F caption="CRACK" > /dev/null &';
 $zidane  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/AXry.gif" -F caption="CIDAN, segundos antes de que metiera gol" > /dev/null &';
-$galletas  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/galletas.png" -F caption="" > /dev/null &';
+$galletas  = 'curl -s -X POST "https://api.telegram.org/bot1378072574:AAHH6x_CubuHc7sjIRWDBaLsei8gZeyBkYE/sendPhoto?chat_id='.$chatId.'" -F photo="https://telegrambottetuan.herokuapp.com/animales/galletas.png" -F caption=" " > /dev/null &';
 
 
 
@@ -167,7 +167,7 @@ switch ($command) {
   
  case 'https://bit.ly/2I73ekY':
  case 'Https://bit.ly/2I73ekY':
-   $response="Mira a ver que te sugiere esa web...
+   $response="Mira a ver que te sugiere esa web...";
    sendMessage($chatId, $response);
    echo exec ($galletas);
 
