6595d93a1c91444f50f479a9a68a27120a319ee5
luisbueno8 <51271382+luisbueno8@users.noreply.github.com>
2022-10-09 12:25:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -91,12 +91,10 @@ case 'Sudo su':
  case 'OCHO':
  case 'Ocho':
    $response="Respuesta correcta, ya tienes la letra O";
-         sendMessage($chatId, $response);
+   sendMessage($chatId, $response);
    $response="🇴":
    sendMessage($chatId, $response);
    sleep(4);
-   $response="2. El nombre de dominio que corresponde a la IP 188. 165. 131. 65";
-   sendMessage($chatId, $response);
   break;
    
 
